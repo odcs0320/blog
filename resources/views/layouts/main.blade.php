b24cbd83a7eba572c08e502adbcfa68b0f0f159e
odcs0320 <odcs0320@msn.com>
2022-12-05 16:27:19 +0800

    20221205 HW第一次修改

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - bootstrap 4 navbar</title>
+  <title>My Waifu by Jim Cheng</title>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'>
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css'><link rel="stylesheet" href="main/style.css">
@@ -11,34 +11,38 @@
 <body>
 <!-- partial:index.partial.html -->
 <nav class="navbar navbar-expand-custom navbar-mainbg">
-        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
+        <a class="navbar-brand navbar-logo" href="#">My Waifu</a>
         <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars text-white"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-upload"></i>Upload</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
+                    <a class="nav-link" href="{{url('photo')}}"><i class="far fa-photo"></i>PhotoA</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>PhotoB</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>PhotoC</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>PhotoD</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>PhotoE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>PhotoF</a>
                 </li>
             </ul>
         </div>
     </nav>
+@yield('photos')
 <!-- partial -->
   <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.main')<!--繼承父視圖-->
+@section('photos')
+
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -11,54 +14,7 @@
 <body>
 <!-- partial:index.partial.html -->
 <div class="gallery" itemscope="" itemtype="http://schema.org/ImageGallery">
-  <figure class="gallery-item horizontal" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/-gS54SWrHMg/2000x1000" itemprop="contentUrl" data-size="2000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%201000%20500'%20%2F%3E" data-src="https://source.unsplash.com/-gS54SWrHMg/1000x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item vertical" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/AU1rKyKPJco/1000x2000" itemprop="contentUrl" data-size="1000x2000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%201000'%20%2F%3E" data-src="https://source.unsplash.com/AU1rKyKPJco/500x1000" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Hello from Poland</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/AXfDvKOawZQ/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/AXfDvKOawZQ/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item horizontal" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/gKlkZrsG_Pw/2000x1000" itemprop="contentUrl" data-size="2000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%201000%20500'%20%2F%3E" data-src="https://source.unsplash.com/gKlkZrsG_Pw/1000x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item vertical" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/DVONaLgCRxo/1000x2000" itemprop="contentUrl" data-size="1000x2000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%201000'%20%2F%3E" data-src="https://source.unsplash.com/DVONaLgCRxo/500x1000" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/o7txpYpxNLs/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/o7txpYpxNLs/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Have a nice day</figcaption>
-  </figure>
-  <figure class="gallery-item vertical" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/ZsgUsl8GATg/1000x2000" itemprop="contentUrl" data-size="1000x2000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%201000'%20%2F%3E" data-src="https://source.unsplash.com/ZsgUsl8GATg/500x1000" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">This is a very very long description to show you it's possible to add something like this</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/CkagyZJ88kE/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/CkagyZJ88kE/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item horizontal" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/PpQ4-HOZ_8U/2000x1000" itemprop="contentUrl" data-size="2000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%201000%20500'%20%2F%3E" data-src="https://source.unsplash.com/PpQ4-HOZ_8U/1000x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/si7gjqJQj_8/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/si7gjqJQj_8/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item vertical" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/u0M0gyuexfE/1000x2000" itemprop="contentUrl" data-size="1000x2000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%201000'%20%2F%3E" data-src="https://source.unsplash.com/u0M0gyuexfE/500x1000" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/aQcE3gDSSTY/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/aQcE3gDSSTY/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item vertical" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/GkCafprWKRo/1000x2000" itemprop="contentUrl" data-size="1000x2000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%201000'%20%2F%3E" data-src="https://source.unsplash.com/GkCafprWKRo/500x1000" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/OFlzoTfpRdw/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/OFlzoTfpRdw/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/YlFM0-LdHu8/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/YlFM0-LdHu8/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
-  <figure class="gallery-item" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="https://source.unsplash.com/c_Tc9ZELeYw/1000x1000" itemprop="contentUrl" data-size="1000x1000"><img class="lazyload lazypreload fadein" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http://www.w3.org/2000/svg'%20viewBox%3D'0%200%20500%20500'%20%2F%3E" data-src="https://source.unsplash.com/c_Tc9ZELeYw/500x500" itemprop="thumbnail" alt="Image description"/></a>
-    <figcaption class="gallery-caption" itemprop="caption description">Caption</figcaption>
-  </figure>
+
 </div>
 <!-- Root element of PhotoSwipe. Must have class pswp.-->
 <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
@@ -112,7 +68,9 @@
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.0.2/lazysizes.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe-ui-default.js'></script><script  src="photos/script.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe-ui-default.js'></script><script  src="{{asset('photos/script.js')}}"></script>
 
 </body>
 </html>
+
+@stop
