5bd2fbff46e0fe00f08c19e95871917f281eb794
JimCheng0310 <odcs0320@msn.com>
2022-12-08 19:27:52 +0800

    202212081927

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-@extends('layouts.master')<!--繼承父視圖-->
-@section('gallery')
-
-{!! Form::open(['url'=>'articles','method'=>'POST','files'=>true]) !!}
-
-        {!! Form::hidden('mode',1)!!}
-=======
 <body style="text-align: center">
 <h1>匿名貼圖討論區</h1>
 
@@ -17,7 +9,6 @@
         {!! Form::submit('送出', []) !!}
 
         <br><br>
->>>>>>> 35361c8186b705086d410a44bffe5d5d56953f53
 
         {!! Form::label('subject', '標題', ['class' => 'myclass']) !!}
         {!! Form::text('subject', null) !!}
@@ -41,38 +32,6 @@
 
         <br><br>
 
-<<<<<<< HEAD
-        {!! Form::label('sort', '排序', []) !!}
-        {!! Form::number('sort', rand(0,255), ['min'=>0 , 'max'=>255]) !!}<br><br>
-
-        {!! Form::label('month', '出生月份', []) !!}
-        {!! Form::selectMonth('month', null, []) !!}
-
-        {!! Form::label('day', '出生日期', []) !!}
-        {!! Form::selectRange('day', 1, 31, 1, []) !!}<br><br>
-
-        {!! Form::label('tags', '角色職業', []) !!}
-        {!! Form::select('tags', $tags ,null, ['placeholder' => '請選擇角色職業']) !!}<br><br>
-
-        {!! Form::label('pwd', '密碼', []) !!}
-        {!! Form::password('pwd', []) !!}<br><br>
-
-        {!! Form::label('email', 'Email', []) !!}
-        {{ Form::email('email',null,[]) }}<br><br>
-
-        {!! Form::label('pic', '圖片', []) !!}
-        {!! Form::file('pic', []) !!}<br><br>
-
-        {!! Form::label('date', '日期', []) !!}
-        {!! Form::date('date',null, []) !!}<br><br>
-
-        {!! Form::submit('儲存', []) !!}
-        {!! Form::reset('重置', []) !!}
-
-{!! Form::close() !!}
-@stop
-=======
 
 {!! Form::close() !!}
 </body>
->>>>>>> 35361c8186b705086d410a44bffe5d5d56953f53
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,53 +2,6 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-<<<<<<< HEAD
-  <title>CodePen - Dashboard</title>
-  <link rel="stylesheet" href="main/style.css">
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<span class='bckg'></span>
-<header>
-  <h1>
-    Juan C
-  </h1>
-  <nav>
-    <ul>
-      <li>
-        <a data-title='Projects'>Unidad 1</a>
-      </li>
-      <li>
-        <a data-title='Team'>Unidad 2</a>
-      </li>
-      <li>
-        <a data-title='Diary'>UNidad 3</a>
-      </li>
-      <li>
-        <a data-title='Timeline'>Unidad 4</a>
-      </li>
-      <li>
-        <a data-title='Settings'>Unidad 5</a>
-      </li>
-      <li>
-        <a data-title='Search'>Unidad 6</a>
-      </li>
-    </ul>
-  </nav>
-</header>
-<main>
-  <div class='title'>
-    <h2>Projects</h2>
-    <a href='#' title='Profil'>
-      Hello Bob !
-    </a>
-  </div>
-  <article class='larg'></article>
-</main>
-<!-- partial -->
-  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="{{asset('main/script.js')}}"></script>
-=======
   <title>JimCheng</title>
   <link rel="stylesheet" href="{{asset('css/komicamenustyle.css')}}">
 </head>
@@ -69,7 +22,6 @@
 @yield('content')
 </div>
 <!-- partial -->
->>>>>>> 35361c8186b705086d410a44bffe5d5d56953f53
 
 </body>
 </html>
