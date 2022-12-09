7d6465493e46fe9d5dfebe898c608966c118406d
odcs0320 <odcs0320@msn.com>
2022-12-09 16:36:02 +0800

    20221209 上課

--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -1,76 +1,215 @@
-@extends('layouts.main')<!--繼承父視圖-->
-@section('photos')
-
 <!DOCTYPE html>
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Magnific Gallery V2</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.css'>
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/default-skin/default-skin.min.css'><link rel="stylesheet" href="photos/style.css">
+  <title>CodePen - 4chan</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/fourchanstyle.css">
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="gallery" itemscope="" itemtype="http://schema.org/ImageGallery">
-
-</div>
-<!-- Root element of PhotoSwipe. Must have class pswp.-->
-<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-  <!--
-  Background of PhotoSwipe.
-  It's a separate element as animating opacity is faster than rgba().
-  -->
-  <div class="pswp__bg"></div>
-  <!-- Slides wrapper with overflow:hidden.-->
-  <div class="pswp__scroll-wrap">
-    <!--
-    Container that holds slides.
-    PhotoSwipe keeps only 3 of them in the DOM to save memory.
-    Don't modify these 3 pswp__item elements, data is added later on.
-    -->
-    <div class="pswp__container">
-      <div class="pswp__item"></div>
-      <div class="pswp__item"></div>
-      <div class="pswp__item"></div>
-    </div>
-    <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed.-->
-    <div class="pswp__ui pswp__ui--hidden">
-      <div class="pswp__top-bar">
-        <!-- Controls are self-explanatory. Order can be changed.-->
-        <div class="pswp__counter"></div>
-        <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-        <button class="pswp__button pswp__button--share" title="Share"></button>
-        <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-        <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-        <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR-->
-        <!-- element will get class pswp__preloader--active when preloader is running-->
-        <div class="pswp__preloader">
-          <div class="pswp__preloader__icn">
-            <div class="pswp__preloader__cut">
-              <div class="pswp__preloader__donut"></div>
-            </div>
-          </div>
+<body>
+  <!-- Header -->
+  <header>
+    <img src="https://i.imgur.com/aiErE1z.jpg">
+    <h1>&#x2Frschan&#x2F</h1>
+    <hr>
+  </header>
+  <!-- Submission -->
+  <section>
+    <form action="">
+      <div>
+        <label for="">Name</label>
+        <input type="text" placeholder="Anonymous">
+      </div>
+      <div>
+        <label for="">Subject</label>
+        <input type="text">
+        <input type="submit" value="Post" tabindex="6">
+      </div>
+      <div>
+        <label for="">Comment</label>
+        <textarea cols="48" rows="4" wrap="soft" tabindex="4"></textarea>
+      </div>
+      <div>
+        <label for="">File</label>
+        <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">
+      </div>
+    </form>
+    <hr>
+  </section>
+  <!-- Threads -->
+  <main>
+    <div class="thread">
+      <!-- OP -->
+      <div class="op">
+        <div class="file">
+          <div class="file-info">37842834.png (37 KB, 640x480)</div>
+         <img src="https://cdn.roland.com/assets/images/products/main/sh-101_plug-out_grey_main.jpg">
         </div>
+        <span class="thread-info">
+          <p class="title">
+            /sh101/
+          </p>
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+          <p>[<a href="">Reply</a>]</p>
+        </span>
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum nulla erat, nec auctor ante hendrerit ut.</blockquote>
       </div>
-      <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-        <div class="pswp__share-tooltip"></div>
+      <a>+6 posts omitted.</a>
+      <!-- Replies -->
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <div class="file">
+            <div class="file-info">Cover.jpg  (537 KB, 1706x1706) </div>
+            <img src="https://dt7v1i9vyp3mf.cloudfront.net/styles/news_large/s3/imagelibrary/s/synthrolandsh101-0701-JPwzkY0H9QIkQgWxdT.STgpEF30ADgrA.jpg">
+          </div>
+        <blockquote>Lorem ipsum dolor sit amet.</blockquote>
       </div>
-      <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
-      <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
-      <div class="pswp__caption">
-        <div class="pswp__caption__center"></div>
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <div class="file">
+            <div class="file-info">Cover.jpg  (537 KB, 1706x1706) </div>
+            <img src="https://d29rinwu2hi5i3.cloudfront.net/article_media/ec55ee53-bbed-4d6b-b757-8c1cfad43d27/w768/ms101-1.jpg">
+          </div>
+        <blockquote>Nulla nec nibh vitae eros sollicitudin consequat quis vitae purus. Phasellus ornare convallis volutpat. Donec nulla magna, aliquam vitae dolor sit amet, egestas auctor dui.</blockquote>
       </div>
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <blockquote>Aliquam eget augue et mauris imperdiet consectetur quis ut massa. Sed nec magna nunc.</blockquote>
+      </div>
+      <hr>
     </div>
-  </div>
-</div>
+    <div class="thread">
+      <!-- OP -->
+      <div class="op">
+        <div class="file">
+          <div class="file-info">37842834.png (37 KB, 640x480)</div>
+          <img src="https://lh6.googleusercontent.com/1YWK_esg5Rlp-1mfBnR5KPpEVMkGeAOvzzpdyTFcgy8vAHQEQ1_tdlOEA4AURbl2HdHconJlawSchty2QZ9Htp4LRCzBWM4Cm4pY6QWguq1gU7idhPZAP_gLrrvYi0Q9YA=s412">
+        </div>
+        <span class="thread-info">
+          <p class="title">
+            /monomachine/
+          </p>
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+          <p>[<a href="">Reply</a>]</p>
+        </span>
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum nulla erat, nec auctor ante hendrerit ut.</blockquote>
+      </div>
+      <a>+6 posts omitted.</a>
+      <!-- Replies -->
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <div class="file">
+            <div class="file-info">Cover.jpg  (537 KB, 1706x1706) </div>
+            <img src="https://farm6.staticflickr.com/5506/12296992446_082a7cb048_c.jpg">
+          </div>
+        <blockquote>Lorem ipsum dolor sit amet.</blockquote>
+      </div>
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <div class="file">
+            <div class="file-info">Cover.jpg  (537 KB, 1706x1706) </div>
+            <img src="https://images.reverb.com/image/upload/s--zJpnnwvp--/f_auto,t_large/v1584929163/fmk0pidyyxy2gs2rogd6.jpg">
+          </div>
+        <blockquote>Nulla nec nibh vitae eros sollicitudin consequat quis vitae purus. Phasellus ornare convallis volutpat. Donec nulla magna, aliquam vitae dolor sit amet, egestas auctor dui.</blockquote>
+      </div>
+      <div class="reply">
+        <input type="checkbox">
+        <span class="thread-info">
+          <p class="name">
+            Anonymous
+          </p>
+          <p>
+            06/10/20(Wed)18:05:13
+          </p>
+          <p>
+            No. 76298902
+          </p>
+        </span>
+        <blockquote>Aliquam eget augue et mauris imperdiet consectetur quis ut massa. Sed nec magna nunc.</blockquote>
+      </div>
+      <hr>
+    </div>
+  </main>
+  <!-- Footer -->
+  <footer>
+    All trademarks and copyrights on this page are owned by their respective parties. Images uploaded are the responsibility of the Poster. Comments are owned by the Poster.
+  </footer>
+</body>
 <!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.0.2/lazysizes.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe-ui-default.js'></script><script  src="{{asset('photos/script.js')}}"></script>
+  <script  src="css/fourchanscript.js"></script>
 
 </body>
 </html>
-
-@stop
